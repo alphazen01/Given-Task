4a7edb57b3ec694034e493259a8091394563a1a7
alphazen01 <parvejmahmud555@gmail.com>
2024-07-14 22:22:19 -0700

    ..

--- lib/mahmud/home.dart
+++ lib/mahmud/home.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class LocalHome extends StatelessWidget {
+  const LocalHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
