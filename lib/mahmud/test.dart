0436f77bfb05b0bb4cd9dccaf3f13b26125e9d71
alphazen01 <parvejmahmud555@gmail.com>
2024-07-14 22:13:33 -0700

    ...

--- lib/mahmud/test.dart
+++ lib/mahmud/test.dart
@@ -5,6 +5,10 @@ class TestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("This is mahmud branch"),
+      ),
+    );
   }
 }
